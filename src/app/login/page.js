37974f1fe409cb137fbbeaf71feaf32e9d9b9ee9
minivetsystem <minivetsystem@gmail.com>
2025-05-13 12:03:03 +0530

    logo

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,15 +1,15 @@
-'use client'
+"use client";
 
-import React, { useState } from 'react';
-import Link from 'next/link';
-import { useRouter } from 'next/navigation'
-import "../globals.css"
+import React, { useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import "../globals.css";
 export default function Login() {
-  const router = useRouter()
+  const router = useRouter();
 
   const [formData, setFormData] = useState({
-    email: '',
-    password: '',
+    email: "",
+    password: "",
   });
   const [loading, setLoading] = useState(false);
 
@@ -17,9 +17,8 @@ export default function Login() {
     e.preventDefault();
     try {
       setLoading(true);
-    
+
       console.log("Login success", response.data);
-   
     } catch (err) {
       console.error("Login error", err);
     } finally {
@@ -32,7 +31,12 @@ export default function Login() {
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="flex gap-2 flex-col w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
           <div className="pt-6 w-full m-auto flex items-center justify-center text-2xl font-semibold text-primary-midnightBlue">
-            <img src="/logo2.png" className="mr-3 h-6 sm:h-9" alt="Kirat IT Solution" title='Kirat IT Solution' />
+            <img
+              src="/kirat learning logo_color.jpg"
+              className="mr-3 h-6 sm:h-9"
+              alt="Kirat IT Solution"
+              title="Kirat IT Solution"
+            />
           </div>
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
@@ -40,7 +44,12 @@ export default function Login() {
             </h1>
             <form className="space-y-4 md:space-y-6" onSubmit={onLogin}>
               <div>
-                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Your email
+                </label>
                 <input
                   type="email"
                   name="email"
@@ -48,12 +57,19 @@ export default function Login() {
                   className="bg-gray-50 border border-gray-300 text-primary-midnightBlue rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                   placeholder="name@company.com"
                   value={formData.email}
-                  onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                  onChange={(e) =>
+                    setFormData({ ...formData, email: e.target.value })
+                  }
                   required
                 />
               </div>
               <div>
-                <label htmlFor="password" className="block mb-2 text-sm font-medium text-primary-midnightBlue">Password</label>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-primary-midnightBlue"
+                >
+                  Password
+                </label>
                 <input
                   type="password"
                   name="password"
@@ -61,15 +77,25 @@ export default function Login() {
                   className="bg-gray-50 border border-gray-300 text-primary-midnightBlue rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                   placeholder="••••••••"
                   value={formData.password}
-                  onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+                  onChange={(e) =>
+                    setFormData({ ...formData, password: e.target.value })
+                  }
                   required
                 />
               </div>
               <div className="flex items-center justify-between">
-                <Link href="#" className="text-sm font-medium text-primary-600 hover:underline">Forgot password?</Link>
+                <Link
+                  href="#"
+                  className="text-sm font-medium text-primary-600 hover:underline"
+                >
+                  Forgot password?
+                </Link>
               </div>
-              <button type="submit" className="w-full text-white bg-primary-blue hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
-                {loading ? 'Signing in...' : 'Sign in'}
+              <button
+                type="submit"
+                className="w-full text-white bg-primary-blue hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+              >
+                {loading ? "Signing in..." : "Sign in"}
               </button>
             </form>
           </div>
