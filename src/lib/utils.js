f1a4c268a66b2b017a80f270ae0ed13a328a32b9
minivetsystem <minivetsystem@gmail.com>
2025-06-02 20:54:33 +0530

    update

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -5,8 +5,4 @@ export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
 
-export function generateUniqueId() {
-  const timestamp = Date.now().toString(36)
-  const randomStr = Math.random().toString(36).substring(2, 8)
-  return `REG_${timestamp}_${randomStr}`.toUpperCase()
-}
+
