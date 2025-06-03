7a1b5756de00b6802dc9312979df972e03d20a3b
minivetsystem <minivetsystem@gmail.com>
2025-06-03 20:46:11 +0530

    development

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -72,8 +72,8 @@ export default function LoginForm() {
       const data = await response.json()
 
       if (!response.ok) {
-        throw new Error(data.error || "Login failed")
-      }
+  throw new Error(data.error || "Login failed");
+}
 
     
 
--- src/components/dasboadHeader/DashboardHeader.js
+++ src/components/dasboadHeader/DashboardHeader.js
@@ -9,36 +9,40 @@ import {
 } from "@/components/ui/popover";
 import { ProfileValidator } from "./ProfileValidator";
 import { Button } from "../ui/button";
-
-
+import axios from "axios";
+import { useAuth } from "../auth/AuthProvider";
 
 export default function DashboardHeader() {
   const router = useRouter();
+  const { logout} = useAuth();
   const [isModel, setIsModel] = useState(false);
-  const [isDark, setIsDark] = useState(false)
+  const [isDark, setIsDark] = useState(false);
 
   const handleModel = () => {
     setIsModel(!isModel);
   };
 
-  const logout = async () => {
-    try {
-      await axios.get("/api/auth/logout");
-      router.push("/login");
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
+  const hangleLogout = async () => {
+ 
+logout();
+  router.push("/login");
+};
+
 
   return (
     <>
       <header className="flex flex-none overflow-hidden w-full px-2 py-2 justify-between border border-gray-200">
         <div className="flex gap-2 items-center">
-        <img src="/Logo.png" className="mr-3 h-14" alt="Kirat IT Solution" title='Kirat IT Solution' />
+          <img
+            src="/Logo.png"
+            className="mr-3 h-14"
+            alt="Kirat IT Solution"
+            title="Kirat IT Solution"
+          />
         </div>
         <div className="flex gap-2 items-center">
-        {/* <Switch checked={isDark} onCheckedChange={setIsDark}/> */}
-        {/* <Popover >
+          {/* <Switch checked={isDark} onCheckedChange={setIsDark}/> */}
+          {/* <Popover >
           <PopoverTrigger>
           <Avatar>
             <AvatarImage src="https://github.com/shadcn.png" />
@@ -61,14 +65,14 @@ export default function DashboardHeader() {
             </div>
           </PopoverContent>
         </Popover> */}
-        {/* <ProfileValidator/> */}
+          {/* <ProfileValidator/> */}
         </div>
         <Button
-                className="block px-4 py-2   text-left hover:bg-red-500 "
-                onClick={logout}
-              >
-                Log out
-              </Button>
+          className="block px-4 py-2   text-left hover:bg-red-500 "
+          onClick={hangleLogout}
+        >
+          Log out
+        </Button>
       </header>
     </>
   );
